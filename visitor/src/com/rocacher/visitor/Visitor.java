619b73b2cb51252868b9cbdcb1a8e7ce48f8a89e
Tamara Rocacher <tamara.rocacher@gmail.com>
2018-05-04 22:27:26 +0200

    Revert "tp1: arborescence"
    This reverts commit eb257f218c46047c0408d6c68fb92b1623bc6113.

--- visitor/src/com/rocacher/visitor/Visitor.java
+++
@@ -1,9 +0,0 @@
-package com.rocacher.visitor;
-
-public abstract class Visitor {
-    protected Visitor(){};
-    public abstract void visiteFile (File f);
-    public abstract void visiteLnk (Link lnk);
-    public abstract void visiteDir (Directory d);
-    public abstract void visiteSymlnk(Symlink slnk);
-}
