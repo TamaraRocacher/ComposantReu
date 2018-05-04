29509a891ea0fef07bf8488dcf3a2c9318694faf
Tamara Rocacher <tamara.rocacher@gmail.com>
2018-05-04 22:42:12 +0200

    for merge

--- visitor/src/com/rocacher/visitor/Symlink.java
+++ visitor/src/com/rocacher/visitor/Symlink.java
@@ -26,11 +26,7 @@ public class Symlink extends ElementStockage
         else return reference.size()  ;
     }
 
-    //public void cat()
-    //affiche le symbole du lien puis l'adresse absoule de sa rfrence
-    //{
-    //    System.out.println("ln -s "+reference.absoluteAdress()+" "+name) ;
-    //}
+    
 
     public void accept(Visitor v){
         v.visiteSymlnk(this);
